70eb5c7e86164302463f5d18ce14bfe655c5db90
Jesse Kevon <jessekevon@gmail.com>
2015-07-14 16:51:40 -0500

    initial setup

---
+++ wp-content/themes/specht/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+
+				<footer id="colophon" class="site-footer" role="contentinfo">
+					<div class="wrap">
+						<p>Website by what.it.is</p>
+					</div>
+				</footer><!-- #colophon -->
+				
+</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+	<!--  
+	<script src="<?php echo get_template_directory_uri(); ?>/js/slick.js"></script>
+	-->
+</body>
+</html>
---
+++ wp-content/themes/specht/page-templates/homepage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Homepage
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+	<h1>Home</h1>
+	
+<?php
+get_footer();
